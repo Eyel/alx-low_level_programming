28535b1e2892da4cd4e10acd35f114815eff1efe
AMI <leye.a@live.fr>
2022-12-21 14:10:53 -0800

    print alpbt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/* more headers goes there */
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: always (0) success
+ */
+int main(void)
+{
+	char ch;
+	ch = 'a';
+	do {
+		if ((ch != 'q') && (ch != 'e')) 
+		{
+			putchar(ch);
+			ch++;
+		}
+		
+	} while (ch <= 'z');
+
+	putchar('\n');
+
+	return (0);
+}
