f687329ac6923eddfa2bf8cb7addd1b3bc920f00
AMI <leye.a@live.fr>
2022-12-21 14:00:05 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: always (0) success
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	do {
+		putchar(ch);
+		ch++;
+	} while (ch <= 'z');
+
+
+	ch = 'A';
+	do {
+	
+		putchar(ch);
+		ch++;
+	} while (ch <= 'Z');
+
+	putchar('\n');
+		ch++;
+
+	return (0);
+}
