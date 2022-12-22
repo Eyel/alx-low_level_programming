442708b32bf227f2305e3d78f63c9e2ec656fef2
AMI <leye.a@live.fr>
2022-12-22 15:32:23 -0800

    5 rev str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,9 +15,7 @@ char* print_rev(char *s)
 {
 	const int len = strlen(s);
 	int i;
-	char *rev;
-
-	rev = malloc(sizeof(*char));
+	char *rev = malloc(sizeof(char));
 	
 	for(i = len - 1; i >= 0; i--)
 		rev[len - i - 1] = s[i];
