e68f026455645b9b410d8f1c5a98d92fed0c685a
AMI <leye.a@live.fr>
2022-12-22 06:16:42 -0800

    1 swap int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,8 @@
 /* betty style doc for function main goes there */
 /**
  * swap_int - is n pos neg or zero
- *
+ *@a: pointer to be swapped
+ *@b: pointer to be swapped
  * Description: swap  int
  *
  * Return: void
