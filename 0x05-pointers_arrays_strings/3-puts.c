57118eeb3fcb277053e556d65e3593b2071b8019
AMI <leye.a@live.fr>
2022-12-22 07:07:47 -0800

    3 puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <string.h>
+/* betty style doc for function main goes there */
+/**
+ * _puts - display string
+ * @s: string
+ * Description: string display
+ *
+ * Return: void
+ *
+ */
+
+void _puts(char *s)
+{
+	puts(s)
+}
