643d010c482d7a5af957f43d43424fb16f39ee4a
AMI <leye.a@live.fr>
2022-12-22 05:34:38 -0800

    0 reset 98

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 /* more headers goes there */
 
+#include <stdlib.h>
+void reset_to_98(int *n);
 /* betty style doc for function main goes there */
 /**
  * main - is n pos neg or zero
@@ -8,6 +11,16 @@
  *
  * Return: always (0) success
  */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
 
 void reset_to_98(int *n)
 {
