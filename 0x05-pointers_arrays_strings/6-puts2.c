e9483456863adab1006ccd457cf10298f2ed55f1
AMI <leye.a@live.fr>
2022-12-22 16:01:34 -0800

    6 puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* betty style doc for function main goes there */
+/**
+ * puts2 - print every other char
+ * @s: string
+ * Description: print evry other char
+ *
+ * Return: void
+ *
+ */
+
+void puts2(char *s)
+{
+	const int len = strlen(s);
+	int i;
+	char *rev = malloc(sizeof(char));
+
+	for (i = 0; i < len; i = i + 2)
+		puts(s[i]);
+	/* *s = *rev; */
+	puts('\n');
+}
