6a7160bb03d2d3fd3ff81ed682d825c3400fc4e1
AMI <leye.a@live.fr>
2022-12-26 06:25:19 -0800

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int _abs(int n);
+/**
+ * _abs - main entry
+ * @n: int to test
+ *
+ * Description: return abs value
+ *
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@ void _putchar(char n);
 int _islower(char n);
 int _isalpha(char n);
 int print_sign(int n);
+int _abs(int n);
