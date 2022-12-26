193d13828260fa2539f6e224b463b72db20b6c34
AMI <leye.a@live.fr>
2022-12-26 14:27:48 -0800

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int add(int a, int b);
+/**
+ * add - main entry
+ * @a : int
+ * @b: int
+ * Description: add a and b
+ * 
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
