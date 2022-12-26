ad51ea796c7e65f283c6674289f54948cabd23eb
AMI <leye.a@live.fr>
2022-12-26 14:41:37 -0800

    11 to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,24 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 98; i++)
+	if ( n < 98)
 	{
-		_putchar('0' + i);
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i < 98; i++)
+		{
+			_putchar('0' + i);
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			_putchar('0' + i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('0' + 98);
 	_putchar('\n');
 }
