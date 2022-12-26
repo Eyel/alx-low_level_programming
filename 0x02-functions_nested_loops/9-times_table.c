c0dccba30e8c534d9dcefc5255d5f4ad6e07abd6
AMI <leye.a@live.fr>
2022-12-26 13:56:34 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,25 +26,26 @@ void times_table(void)
 			{
 				_putchar('0' + n / 10);
 				_putchar('0' + n % 10);
+				_putchar(',');
 			}
 			else
 			{
 				_putchar('0' + n);
+				_putchar(',');
 				_putchar(' ');
 			}
 			_putchar(' ');
-			_putchar(',');
 		}
 		n = table * 9;
 		if (n > 10)
 		{
 			_putchar('0' + n / 10);
 			_putchar('0' + n % 10);
+
 		}
 		else
 		{
 			_putchar('0' + n);
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
