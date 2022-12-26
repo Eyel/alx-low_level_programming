4601b2ff279752696cee3f81d7bf7c9a8ecd74b2
AMI <leye.a@live.fr>
2022-12-26 05:29:39 -0800

    3 is lowerC

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int _islower(char n);
+/**
+ * _islower - main entry
+ *
+ * Description: tells if a char is lowercase
+ *
+ * Return: int
+ */
+
+
+int _islower(char n);
+{
+	return (islower(n));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,4 @@
-/**
- * print_alphabet - print alphabet
- *
- * Description: print
- *
- * Return: void
- */
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void _putchar(char n);
+int _islower(char n);
