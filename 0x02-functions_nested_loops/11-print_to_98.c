4168940f0bcb0d53fd55efee2e160f207ce3e78d
AMI <leye.a@live.fr>
2022-12-26 14:35:45 -0800

    11 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void print_to_98(int n);
+/**
+ * print_to_98 - main entry
+ *
+ * Description: print up to 98
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++)
+	{
+		_putchar('0' + i);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
