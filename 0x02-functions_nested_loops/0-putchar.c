7dde0931444e3d17480b7d756a7d6e1de2060082
AMI <leye.a@live.fr>
2022-12-26 04:33:31 -0800

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - main entry
+ *
+ * Description: print putchar
+ *
+ * Return: void
+ */
 
-
-int main( void )
+int main(void)
 {
 	printf("_putchar\n");
 	return (0);
