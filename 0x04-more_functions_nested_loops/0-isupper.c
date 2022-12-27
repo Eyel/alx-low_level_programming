b55b431a42aa9ec5453db716f400d459472bd654
AMI <leye.a@live.fr>
2022-12-26 16:14:46 -0800

    0 isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * _isupper - upper char
+ *@c: char
+ * Return: int
+ *
+ */
+
+int _isupper(char c)
+{
+	if (isupper(c) == 0)
+		return (0);
+	else
+		return (1);
+}
