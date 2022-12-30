36c58231d62b41ab9e24cf38a4d542643218627e
AMI <leye.a@live.fr>
2022-12-30 09:16:53 -0800

    2 mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <ctype.h>
+/* betty style doc for function main goes there */
+/**
+ * mul - multiply
+ *@a: int
+ *@b: int
+ * Return: int
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
