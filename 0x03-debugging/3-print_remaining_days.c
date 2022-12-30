49ca29520b059b27bfaf0a2df505fa83d4651a77
AMI <leye.a@live.fr>
2022-12-30 08:38:43 -0800

    3 remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,10 +16,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day > 60)
 		{
-			printf("---");
 			day++;
-		}
-	
+		}	
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
