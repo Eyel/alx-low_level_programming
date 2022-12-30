de0568186533fbbd7ce1a32eaf7d4a8b3bc66a2a
AMI <leye.a@live.fr>
2022-12-30 09:57:16 -0800

    5 more num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,7 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
-			putchar('0' + j);
+			printf("%d", j); /* putchar('0' + j);*/
 		putchar('\n');
 	}
 	putchar('\n');
