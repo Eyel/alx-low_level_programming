3d722903abb9606d5e86d84b71e385c2e2be5986
AMI <leye.a@live.fr>
2022-12-30 12:08:26 -0800

    100 comb 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/* more headers goes there */
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: always (0) success
+ */
+int main(void)
+{
+	int i;
+	int j;
+	
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i+1; j <= 9; j++)
+		{
+			if (!( i == 0 && j == 1))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar('0' + i);
+			putchar('0' + j);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
