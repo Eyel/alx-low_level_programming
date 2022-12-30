0faf8f4ccc18f86885b3c8e1a8351f6ba711dc8b
AMI <leye.a@live.fr>
2022-12-30 10:39:06 -0800

    9 fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * fizz_buzz- fixx buzz and num
+ * Return: void
+ *
+ */
+
+void fizz_buzz(void);
+{
+	int i;
+
+	printf("1");
+	for (i = 2; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf(" Fizz");
+		else if (i % 3 != 0 && i % 5 == 0)
+			printf(" Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else
+			printf(" %d", i);
+	}
+	putchar('\n');
+}
