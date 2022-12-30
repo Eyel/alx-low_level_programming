9d0b24d80932ec6ca9500bcaab0da1bffb7c3502
AMI <leye.a@live.fr>
2022-12-30 10:12:41 -0800

    7 print diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * print_diagonal- draw diagonal line
+ * @n: int
+ * Return: void
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n-1; j++)
+			putchar(' ');
+		pucthar('\\');
+		putchar('\n');
+	}
+}
