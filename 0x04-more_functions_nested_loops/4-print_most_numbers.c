74ea2730a391163d9e05818cc3b202d39b7df256
AMI <leye.a@live.fr>
2022-12-30 09:27:49 -0800

    4 print 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* betty style doc for function main goes there */
+/**
+ * print_most_numbers- print num from 0 to 9
+ * Return: void
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 || i != 4)
+			putchar('0' + i);
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int _isupper(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif /* MAIN_H */
