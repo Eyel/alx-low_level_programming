8eb203d52ebb74c24aff3cdc748f89992636bfa4
AMI <leye.a@live.fr>
2023-01-03 02:03:55 -0800

    2 str chr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+char *_strchr(char *s, char c);
+/**
+ * _strchr - main entry
+ * @s: string input
+ * @c: char to search for
+ * Description: search c in s
+ *
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	char **s2;
+	int i = 0;
+
+	s2 = &s;	
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			*s2 = &s[i];
+			break;
+		}
+		else
+			i++;
+	}
+	return (*s2);
+}
