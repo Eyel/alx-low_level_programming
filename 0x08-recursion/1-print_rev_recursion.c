ea102c01d95094c24107e518a48a825aa9785f92
AMI <leye.a@live.fr>
2023-01-04 02:20:52 -0800

    1 reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void _print_rev_recursion(char *s);
+/**
+ * _print_rev_recursion - main entry
+ * @s: str
+ * Description: print string in reverse
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_puts_recursion(s + 1);
+		_putchar(s[0]);
+	}
+}
