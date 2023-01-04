d967f4150794775d3ac97d3c824816e52af98f42
AMI <leye.a@live.fr>
2023-01-04 02:12:15 -0800

    0 puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void _puts_recursion(char *s);
+/**
+ * _puts_recursion - main entry
+ * @s: str
+ * Description: fill memory with b, n times
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
