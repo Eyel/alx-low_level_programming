e383ab1c89062051e59c76f1e7fbe395401e3e7c
AMI <leye.a@live.fr>
2023-01-04 01:20:51 -0800

    7 print chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,12 +13,15 @@ void print_chessboard(char (*a)[8]);
 
 void print_chessboard(char (*a)[8])
 {
-  for (int i = 0; i < 8 ; i++)
-  {
-    for (int j = 0; j < 8 ; j++)
-    {
-      printf("%c", a[i][j]);
-    }
-    printf("\n");
-  }
+	int i = 0;
+	int j = 0;
+	
+	for (i = 0; i < 8 ; i++)
+	{
+		for (j = 0; j < 8 ; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
 }
